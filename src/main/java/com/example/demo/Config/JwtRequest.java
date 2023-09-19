d833be7662d37a8578cb0d849b0de0c81b68e707
Mouad Makboul <mouadmakboul8@gmail.com>
2023-09-19 13:08:51 +0100

    first commit

---
+++ src/main/java/com/example/demo/Config/JwtRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.Config;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class JwtRequest {
+    private String email;
+    private String password;
+}
